d5c0366ba4b7ec01e28841e847b4ca81ed56453e
cecoro2425 <461984.clot@fje.edu>
2026-04-18 18:19:06 +0200

    Añadiendo animaciones y pantalla inicio

---
+++ app/src/main/java/dam/projecte/pj6_2/MenuInicio.java
@@ -0,0 +1,39 @@
+package dam.projecte.pj6_2;
+
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.menu_inicio);
+
+        final TextView titulo = (TextView) findViewById(R.id.tituloInicio);
+
+
+        final AnimatorSet animacio = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.animacio);
+
+        animacio.setTarget(titulo);
+
+        animacio.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator a) {
+                super.onAnimationEnd(a);
+                animacio.start();
+            }
+        });
+
+        animacio.start();
+    }
+}
